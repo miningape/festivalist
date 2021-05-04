4428a730f8ed1d1f4709d724c8612dbecd255d1c
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-05-04 17:08:04 +0200

    Samo for info

---
+++ frontend_base/assets/js/components/info.js
@@ -0,0 +1,29 @@
+class Info extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="info container secondary-color">
+                <h3>
+                    Festival Title
+                </h3>
+                <p>
+                    Type: genre
+                </p>
+                <p>
+                    Location
+                </p>
+                <p>
+                    Lineup
+                </p>
+                <p>
+                    Ticket Price
+                </p>
+            </div>
+        `;
+    }
+}
+
+customElements.define('info-component', Info);
