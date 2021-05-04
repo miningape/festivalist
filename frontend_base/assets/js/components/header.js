b04c9018b4974c1d6b4ca02fff0b3acde9125bb9
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-05-04 17:07:57 +0200

    Update header.js

--- frontend_base/assets/js/components/header.js
+++ frontend_base/assets/js/components/header.js
@@ -6,20 +6,21 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="header container ">
-            <div class="row d-flex justify-content-between">
-                <div class="col d-flex flex-row mt-2">
-                    <i class="fas fa-bars icon-custom change-to-list-view"></i>
-                </div>
+                <div class="row d-flex justify-content-between">
+                    <div class="col d-flex flex-row mt-2">
+                        <i class="fas fa-bars icon-custom change-to-list-view"></i>
+                    </div>
 
-                <div class="col  text-center">
-                    <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
-                </div>  
+                    <div class="col  text-center">
+                        <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
+                    </div>  
 
-                <div class="col d-flex flex-row-reverse mt-2">
-                    <i class="fas fa-search icon-custom search"></i>
+                    <div class="col d-flex flex-row-reverse mt-2">
+                        <i class="fas fa-search icon-custom search"></i>
+                    </div>
                 </div>
             </div>
-        </div>`
+        `;
     }
 }
 
