688df1002fd9d7a97d3a123b660579ac80a12919
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-05-06 19:16:56 +0200

    Update header.js

--- frontend_base/assets/js/components/header.js
+++ frontend_base/assets/js/components/header.js
@@ -1,28 +1,52 @@
+const PRIMARY_PAGE = "primary.html";
+const SECONDARY_PAGE = "secondary.html";
+
+function getOtherPage(page) {
+    switch(page) {
+        case PRIMARY_PAGE:
+            return SECONDARY_PAGE;
+        case SECONDARY_PAGE:
+            return PRIMARY_PAGE;
+    }
+}
+
+function getIcon(page) {
+    switch(page) {
+        case PRIMARY_PAGE:
+            return "fa-bars";
+        case SECONDARY_PAGE:
+            return "fa-map-marker-alt";
+    }
+}
+
+function getHTML(page) {
+    return `
+        <div class="header container ">
+            <div class="row d-flex justify-content-between">
+                <div class="col d-flex flex-row mt-2">
+                    <a href="${getOtherPage(page)}" class="secondary-color">
+                        <i class="fas ${getIcon(page)} icon-custom"></i>
+                    </a>
+                </div>
+
+                <div class="col text-center">
+                    <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
+                </div>  
+
+                <div class="col d-flex flex-row-reverse mt-2">
+                    <i class="fas fa-search icon-custom search"></i>
+                </div>
+            </div>
+        </div>
+    `;
+}
 class PrimaryHeader extends HTMLElement {
     constructor() {
         super();
     }
-    
+
     connectedCallback() {
-        this.innerHTML = `
-            <div class="header container ">
-                <div class="row d-flex justify-content-between">
-                    <div class="col d-flex flex-row mt-2">
-                        <a href="secondary.html" class="secondary-color">
-                            <i class="fas fa-bars icon-custom change-to-list-view"></i>
-                        </a>
-                    </div>
-
-                    <div class="col text-center">
-                        <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
-                    </div>  
-
-                    <div class="col d-flex flex-row-reverse mt-2">
-                        <i class="fas fa-search icon-custom search"></i>
-                    </div>
-                </div>
-            </div>
-        `;
+        this.innerHTML = getHTML(PRIMARY_PAGE);
     }
 }
 
@@ -32,25 +56,7 @@ class SecondaryHeader extends HTMLElement {
     }
     
     connectedCallback() {
-        this.innerHTML = `
-            <div class="header container ">
-                <div class="row d-flex justify-content-between">
-                    <div class="col d-flex flex-row mt-2">
-                        <a href="primary.html" class="secondary-color">
-                            <i class="fas fa-map-marker-alt icon-custom change-to-map-view"></i>
-                        </a>
-                    </div>
-
-                    <div class="col text-center">
-                        <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
-                    </div>  
-
-                    <div class="col d-flex flex-row-reverse mt-2">
-                        <i class="fas fa-search icon-custom search"></i>
-                    </div>
-                </div>
-            </div>
-        `;
+        this.innerHTML = getHTML(SECONDARY_PAGE);
     }
 }
 
