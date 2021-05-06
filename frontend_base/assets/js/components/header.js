a3befbf8075f7354b24cec5412b3d29e09ac1a9f
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-05-06 18:27:25 +0200

    Updated Header
    Split header into 2 classes to accommodate different change view buttons

--- frontend_base/assets/js/components/header.js
+++ frontend_base/assets/js/components/header.js
@@ -1,8 +1,8 @@
-class Header extends HTMLElement {
+class PrimaryHeader extends HTMLElement {
     constructor() {
         super();
     }
-
+    
     connectedCallback() {
         this.innerHTML = `
             <div class="header container ">
@@ -26,4 +26,33 @@ class Header extends HTMLElement {
     }
 }
 
-customElements.define('header-component', Header);
+class SecondaryHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="header container ">
+                <div class="row d-flex justify-content-between">
+                    <div class="col d-flex flex-row mt-2">
+                        <a href="primary.html" class="secondary-color">
+                            <i class="fas fa-map-marker-alt icon-custom change-to-map-view"></i>
+                        </a>
+                    </div>
+
+                    <div class="col text-center">
+                        <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
+                    </div>  
+
+                    <div class="col d-flex flex-row-reverse mt-2">
+                        <i class="fas fa-search icon-custom search"></i>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define('primary-header-component', PrimaryHeader);
+customElements.define('secondary-header-component', SecondaryHeader);
