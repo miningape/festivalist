a83ccc272897e388347dcf3440bab91ca2ebe058
Robert Spralja <56681655+spralja@users.noreply.github.com>
2021-05-06 17:44:48 +0200

    Update header.js

--- frontend_base/assets/js/components/header.js
+++ frontend_base/assets/js/components/header.js
@@ -8,10 +8,12 @@ class Header extends HTMLElement {
             <div class="header container ">
                 <div class="row d-flex justify-content-between">
                     <div class="col d-flex flex-row mt-2">
-                        <i class="fas fa-bars icon-custom change-to-list-view"></i>
+                        <a href="secondary.html" class="secondary-color">
+                            <i class="fas fa-bars icon-custom change-to-list-view"></i>
+                        </a>
                     </div>
 
-                    <div class="col  text-center">
+                    <div class="col text-center">
                         <h1 class="border px-3 pb-1 border-top-0 border-5 border-custom bg-dark"> Festivalist </h1> 
                     </div>  
 
