dc6a965ce9a03412bda44f4ef0a2a5cf41dcb2ae
kyle <miningape@gmail.com>
2021-05-10 20:39:58 +0200

    Added maps API

--- frontend_base/assets/js/main.js
+++ frontend_base/assets/js/main.js
@@ -1,7 +1,19 @@
 (main = () => {
     console.log("Testing")
+
 })()
 
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+
+// Scrolls to top on call
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
