5abf3857800699d142f28f5efda120ba1c4fe985
kyle <miningape@gmail.com>
2021-05-10 20:54:04 +0200

    Added marker to map

--- frontend_base/assets/js/main.js
+++ frontend_base/assets/js/main.js
@@ -3,13 +3,26 @@
 
 })()
 
-let map;
-
+/**
+ * Initializes the map on the screen
+ * 
+ * called through the callback= property on the script element that includes the maps api
+ */
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 0, lng: 0 },
+        zoom: 3,
+    });
+
+    const roskildeFestival = new google.maps.Marker( {
+        position: { lat: 55.616236, lng: 12.079475 },
+        title: "jeff",
+        map: map,
+    } );
+
+    roskildeFestival.addListener("click", () => {
+        console.log("Roskilde Festival Was Clicked")
+    })
 }
 
 
